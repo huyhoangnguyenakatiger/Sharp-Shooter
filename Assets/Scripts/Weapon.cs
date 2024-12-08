b9c7737595e67bd8f256dd24665ca9a165da4d28
huyhoangnguyenakatiger <huyhoangnguyenakatiger@gmail.com>
2024-12-08 22:19:14 +0700

    add animation for weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     StarterAssetsInputs starterAssetsInputs;
-    ParticleSystem muzzleFlash;
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
+    [SerializeField] Animator animator;
+    const string SHOOT_STRING = "Shoot";
     void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
-        muzzleFlash = GetComponentInChildren<ParticleSystem>();
     }
     void Update()
     {
@@ -20,12 +21,13 @@ public class Weapon : MonoBehaviour
     {
         if (!starterAssetsInputs.shoot) return;
         muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
-            starterAssetsInputs.ShootInput(false);
         }
     }
 }
